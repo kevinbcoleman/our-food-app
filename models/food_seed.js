d2b16ffc0e061f919a4fea551793530332df7c69
Kevin B. Coleman <kevinb.coleman@Kevins-MBP.fios-router.home>
2021-05-10 15:20:46 -0400

    food seed

---
+++ models/food_seed.js
@@ -0,0 +1,28 @@
+
+module.exports = [
+  {
+    name: 'Butter Chicken',
+    image: 'https://www.aheadofthyme.com/wp-content/uploads/2016/01/indian-butter-chicken-3.jpg',
+    description: 'Butter chicken originated from Northern India in 1948. Created by Kundan Lal Gujral, you may notice that Butter Chicken, known as murgh makhani — chicken with butter, is similar to British tikka masala. Prepared in a buttery gravy with the addition of cream gives the curry sauce a silky smooth rich texture.'
+  },
+  {
+    name: 'Lasagna',
+    image: 'https://www.cookingclassy.com/wp-content/uploads/2013/03/lasagna15.jpg',
+    description: 'Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù (ground meats and tomato sauce) and other vegetables, cheese (which may include ricotta and parmesan), and seasonings and spices such as garlic, oregano and basil.'
+  },
+  {
+    name: 'Pad Thai',
+    image: 'https://www.kitchensanctuary.com/wp-content/uploads/2019/07/Pad-Thai-square-FS-29.jpg',
+    description: 'Butter chicken originated from Northern India in 1948. Created by Kundan Lal Gujral, you may notice that Butter Chicken, known as murgh makhani — chicken with butter, is similar to British tikka masala. Prepared in a buttery gravy with the addition of cream gives the curry sauce a silky smooth rich texture.'
+  },
+  {
+    name: 'Ramen',
+    image: 'https://hips.hearstapps.com/hmg-prod/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=0.903xw%3A0.760xh%3B0.0297xw%2C0.0123xh&resize=480%3A270',
+    description: 'Ramen (ラーメン) is a noodle soup dish that was originally imported from China and has become one of the most popular dishes in Japan in recent decades.'
+  },
+  {
+    name: 'Cheeseburger',
+    image: 'https://www.foodrepublic.com/wp-content/uploads/2012/03/033_FR11785.jpg',
+    description: 'Classic American cheeseburger with toppings of choice. Ketchup, cheese, lettuce, tomato and onions are customary.'
+  },
+]
--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const PORT = 3000
 
 
 const app = express()
-// // require('dotenv').config()
-// const PORT = process.env.PORT
-// const MONGODB_URI = process.env.MONGODB_URI
-//
-// // const birdsController = require('./controllers/birds_controller.js')
-// app.use(express.json())
-// app.use(express.static('public'))
-// // app.use('/birds', birdsController)
-//
-//
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+// const birdsController = require('./controllers/birds_controller.js')
+app.use(express.json())
+app.use(express.static('public'))
+// app.use('/birds', birdsController)
+
+
 
 app.get('/', (req, res) => {
   res.send("hello World")
@@ -28,20 +28,20 @@ app.listen(PORT, () => {
 })
 
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-// })
-// mongoose.connection.on('error', (err) =>
-//   console.log(
-//     err.message,
-//     'is mongod not running?/Problem with Atlas connection?'
-//   )
-// )
-// // mongoose.connection.on('connected', () => {
-// //   console.log('mongo connected ', MONGODB_URI)
-// // })
-// mongoose.connection.on('disconnected', () => {
-//   console.log('mongo disconnected')
-// })
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+mongoose.connection.on('error', (err) =>
+  console.log(
+    err.message,
+    'is mongod not running?/Problem with Atlas connection?'
+  )
+)
+mongoose.connection.on('connected', () => {
+  console.log('mongo connected ', MONGODB_URI)
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('mongo disconnected')
+})
